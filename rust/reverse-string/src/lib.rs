b21a99752ab07bae3e7528d5967e567f2e78bc72
Will Madison <willmadison@wills-mac-mini.lan>
2023-09-28 16:03:05 -0400

    revised to a more idiomatic rust approach leveraging the power of iterators

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,12 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let characters: Vec<char> = input.chars().collect();
-    let mut reversed = String::new();
-
-    if characters.len() > 0 {
-        for i in (0.. characters.len()).rev() {
-            reversed.push(characters[i]);
-        }
-    }
-
-    return reversed;
+    return input.chars().rev().collect::<String>();
 }
