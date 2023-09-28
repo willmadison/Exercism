8741ac51e4cd3b711b010838aa9b117922b59d13
Will Madison <willmadison@wills-mac-mini.lan>
2023-09-28 16:33:36 -0400

    solving this a bit more succintly

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    return start.checked_add(Duration::seconds(1_000_000_000)).unwrap();
+    start + Duration::seconds(1_000_000_000)
 }
